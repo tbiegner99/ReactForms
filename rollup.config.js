5437896d22e80a0abe3d94f05046fe6b451cdb98
TJ Biegner <tbiegner99@gmail.com>
2021-01-27 20:06:54 -0500

    added commonjs export

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,15 @@ import pack from './package.json';
 
 export default {
   input: './src/main.js',
-  output: {
-    file: pack.module,
-    format: 'es'
-  },
+  output: [
+    {
+      file: pack.module,
+      format: 'es'
+    },
+    {
+      file: pack.main,
+      format: 'cjs'
+    }
+  ],
   plugins: [babel({ babelHelpers: 'runtime', skipPreflightCheck: true })]
 };
