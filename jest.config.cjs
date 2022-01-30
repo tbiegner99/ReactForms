a7d435a707a10a8cdc54f3387525993b7f8f1d10
TJ Biegner <tbiegner99@gmail.com>
2022-01-29 23:27:44 -0500

    coverage only in src

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
   testMatch: ['**/tests/**/*.spec.js'],
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['src/**/*.js'],
   setupFiles: ['./tests/init.js'],
   setupFilesAfterEnv: ['./tests/after-env.js'],
   testEnvironmentOptions: { resources: 'usable' },
@@ -11,12 +11,12 @@ module.exports = {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy'
-  }
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
